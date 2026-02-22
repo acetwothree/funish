3f471e4ed8fda1e35bab54f1d2efa41ef4661a5c
acetwothree <167348768+acetwothree@users.noreply.github.com>
2026-02-22 02:22:39 -0500

    Create simplest possible app - just Hello world

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,9 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
-// import './index.css'; // Commented out to test
 
-console.log('main.tsx is running');
-console.log('Root element:', document.getElementById('root'));
-
-const rootElement = document.getElementById('root');
-if (rootElement) {
-  console.log('Root element found, creating React root');
-  createRoot(rootElement).render(
-    <StrictMode>
-      <App />
-    </StrictMode>,
-  );
-} else {
-  console.error('Root element not found!');
-}
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
